8f94b8036e8456b1982d1c3dcbbfdaa89e8b34d8
Amol Tambe <amol.tambe@deskera.com>
2020-07-16 07:51:18 +0530

    updated conf in prod

--- src/test/java/deskera/web/automation/erp/bvtSG/tests/CreateCustomNumberFormat.java
+++ src/test/java/deskera/web/automation/erp/bvtSG/tests/CreateCustomNumberFormat.java
@@ -25,7 +25,7 @@ public class CreateCustomNumberFormat extends DriverFactory{
 	 * @param URL
 	 */
 	@BeforeClass
-	@Parameters({ "conf", "environment" })
+	@Parameters({ "confProducts", "environment" })
 	public void getConf(String conf, String URL) {
 		confPath = conf;
 		url = URL;
